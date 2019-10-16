d7546e45104c2c0479feaa0673299eb4066739b2
LouieK22 <louie@tinybits.me>
2019-10-16 08:56:23 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Roblox Firestore API
+
+Work in progress, Do not use in production
---
+++ src/CollectionReference.ts
@@ -0,0 +1,18 @@
+import { FirestoreClient } from "./init";
+import { DocumentReference } from "./DocumentReference";
+
+export class CollectionReference {
+	private client: FirestoreClient;
+
+	public path: string;
+
+	public constructor(client: FirestoreClient, collectionPath: string) {
+		this.client = client;
+
+		this.path = collectionPath;
+	}
+
+	public doc(documentPath: string) {
+		return new DocumentReference(this.client, this.path + "/" + documentPath);
+	}
+}
